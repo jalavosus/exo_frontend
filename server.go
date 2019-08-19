1e882f2574a2d6823dc21a9a84e76d022b4f292b
jalavosus <alavosus.james@gmail.com>
2019-08-18 23:01:49 -0400

    Fix up the server to actually work

--- server.go
+++ server.go
@@ -1,13 +1,36 @@
 package main
 
 import (
+       "log"
     "net/http"
+    "strings"
+    "path"
 )
 
-func main() {
+func Handler(publicDir string) http.Handler {
+	handler := http.FileServer(http.Dir(publicDir))
 
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request){
-        http.ServeFile(w, r, "dist/index.html");
-    });
-  http.ListenAndServe(":3000", nil)
+	return http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
+		_path := req.URL.Path
+
+		// static files
+		if strings.Contains(_path, ".") || _path == "/" {
+			handler.ServeHTTP(w, req)
+			return
+		}
+
+		// the all 404 gonna be served as root
+		http.ServeFile(w, req, path.Join(publicDir, "/index.html"))
+	})
+}
+
+func main() {
+     server := &http.Server{
+     	    Addr:	":3003",
+	    Handler:	Handler("./dist"),
+     }
+     err := server.ListenAndServe()
+     if err != nil {
+     	log.Println(err)
+     }
 }
